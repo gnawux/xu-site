d6faf5065ac7914b323b0910b0ff37b0301c42dd
Wang Xu <gnawux@gmail.com>
2016-09-24 16:04:30 +0800

    some tar need specify -z flag
    Signed-off-by: Wang Xu <gnawux@gmail.com>

--- generate.sh
+++ generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [ ! -f caddy ] ; then
-	wget -O - "https://caddyserver.com/download/build?os=linux&arch=amd64&features=git" | tar -xvf -
+	wget -O - "https://caddyserver.com/download/build?os=linux&arch=amd64&features=git" | tar -zxvf -
 	rm -rf init *.txt
 fi
+
+[ ! -f caddy ] && echo "caddy download failed" && exit 1
 
 sudo docker build -t "gnawux/blog:latest" .
 
